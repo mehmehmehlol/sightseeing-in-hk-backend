bf221af4a474c7ff591d7c0b8f8efa15c4674be8
mehmehmehlol <hkmeganlo@gmail.com>
2021-01-06 00:09:29 -0800

    first Heroku deploy

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root 'welcome#index'
   resources :place_tags, only: [:create]
   resources :tags, only: [:index]
   resources :favorites, only: [:index, :create, :destroy]
